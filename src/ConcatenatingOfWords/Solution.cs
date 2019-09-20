5a3544f496f7a3d3e2ec7884c61a8f7a0f97e6f6
Chris <eng.christhuku@gmail.com>
2019-09-21 00:13:33 +0300

    Add project files.

---
+++ src/ConcatenatingOfWords/Solution.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConcatenatingOfWords
+{
+    public class Solution
+    {
+        public int solution(string[] words)
+        {
+            if (words.Count() == 0) return 0;
+            if (words.Count() == 1)
+            {
+                return GetItsCount(words[0]);
+            }
+            List<int> counts = new List<int>();
+            var ms = Permutate<string>(words).Select(a => a.Aggregate((current, next) => $"{current}{next}")).ToList();
+            foreach (var g in ms)
+            {
+                counts.Add(GetItsCount(g));
+            }
+            return counts.Max();
+        }
+        /// This is too slow but it works
+        /// Reduced my score to 44 due to timeouts
+        public int GetItsCount(string word)
+        {
+            string a = "";
+            List<string> vs = new List<string>();
+            if (word.Length == 1) return 1;
+            for (int i = 0; i <= word.Length - 1; i++)
+            {
+                if (string.IsNullOrWhiteSpace(a))
+                {
+                    a += word[i];
+                    if (i == word.Length - 1)
+                    {
+                        vs.Add(a);
+                    }
+                }
+                else
+                {
+                    if (a.Length == 1)
+                    {
+                        if (a[0] == word[i])
+                        {
+                            a += word[i];
+                            if (i == word.Length - 1)
+                            {
+                                vs.Add(a);
+                            }
+                        }
+                        else
+                        {
+                            a = word[i].ToString();
+                            vs.Add(a);
+                        }
+                    }
+                    else
+                    {
+                        if (a[a.Length - 1] == word[i])
+                        {
+                            a += word[i];
+                            if (i == word.Length - 1)
+                            {
+                                vs.Add(a);
+                            }
+                        }
+                        else
+                        {
+                            vs.Add(a);
+                            a = word[i].ToString();
+                            vs.Add(a);
+                        }
+                    }
+                }
+
+            }
+            var mm = vs.OrderByDescending(s => s.Length).FirstOrDefault();
+            var dd = mm.Length;
+
+            return vs.OrderByDescending(s => s.Length).FirstOrDefault().Trim().Length;
+        }
+        public static IEnumerable<IEnumerable<TSource>> Permutate<TSource>(IEnumerable<TSource> source)
+        {
+            var list = source.ToList();
+
+            if (list.Count > 1)
+                return from s in list
+                       from p in Permutate(list.Take(list.IndexOf(s)).Concat(list.Skip(list.IndexOf(s) + 1)))
+                       select Prepend(p, s);
+
+            return new[] { list };
+        }
+        //This filters repetition
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> items)
+        {
+            if (items.Count() > 1)
+            {
+                return items.SelectMany(item => GetPermutations(items.Where(i => !i.Equals(item))),
+                                       (item, permutation) => new[] { item }.Concat(permutation));
+            }
+            else
+            {
+                return new[] { items };
+            }
+        }
+
+        public static IEnumerable<TSource> Prepend<TSource>(IEnumerable<TSource> source, TSource item)
+        {
+
+            yield return item;
+
+            foreach (var element in source)
+                yield return element;
+        }
+    }
+}
---
+++ tests/ConcatenatingOfWords.Test/SolutionTests.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+
+namespace ConcatenatingOfWords.Test
+{
+    public class SolutionTests
+    { 
+        /// Didnt have time to test methods separately as its a timed tests
+        [Theory]
+        [InlineData(new string[] { "aabb", "aaaa", "bbab" }, 6)]
+        [InlineData(new string[] { "xxbxx", "xbx", "x" }, 4)]
+        [InlineData(new string[] { "dd", "bb", "cc", "dd" }, 4)]
+
+        [InlineData(new string[] { "Chris" }, 1)]
+        [InlineData(new string[] { "Chris Thuku" }, 1)]
+        [InlineData(new string[] { }, 0)]
+        [InlineData(new string[] { "Three" }, 2)]
+        [InlineData(new string[] { "aabbbcc" }, 3)]
+        [InlineData(new string[] { "coffee" }, 2)]
+        [InlineData(new string[] { "abaaabbbqqqqqx" }, 5)]
+        [InlineData(new string[] { "ab" }, 1)]
+        public void solution_Returns_ExpectedResults(string[] words, int result)
+        {
+            Solution sln = new Solution();
+            var res = sln.solution(words);
+            Assert.Equal(result, res);
+        }
+    }
+}
